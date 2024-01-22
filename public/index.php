52eb8a73836e7d5e1de0eee126d8850394ffcb28
MelvinLeroux <leroux.melvin@gmail.com>
2024-01-22 17:29:58 +0100

    structural setup

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+require __DIR__.'/../vendor/autoload.php';
+
+
+$router = new AltoRouter();
+
+//! ATTENTION, si la partie fixe (BASE_URI) contient des caractères spéciaux ou des espaces, AltoRouter ne fonctionne pas ! 
